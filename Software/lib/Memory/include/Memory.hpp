0936ff9ed195a9768df271815b3b8223a29d777e
Cuprum77 <blockland70@gmail.com>
2023-05-14 02:33:10 +0200

    added the memory library

---
+++ Software/lib/Memory/include/Memory.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#include "Memory_Registers.hpp"
+
+#define DEFAULT_CLOCK           100000
+#define DEFAULT_PAGE_SIZE       16
+#define DEFAULT_EEPROM_SIZE     512
+#define DEFAULT_WRITE_CYCLE     5
+#define WORD_SIZE               4
+
+
+class Memory
+{
+public:
+    Memory(unsigned int device_address, i2c_inst_t* i2c);
+    
+    bool verifyConnection();
+    int selfTest();
+
+    unsigned int readWord(unsigned int target_address);
+    void writeWord(unsigned int target_address, unsigned int data);
+private:
+    unsigned int write_cycle_time;
+    unsigned int eeprom_addr; 
+    i2c_inst_t*  i2c;
+};
---
+++ Software/lib/Memory/include/Memory_Registers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+// as we store each variable as a word, we need to space them out by 4 bytes
+#define MEMORY_CURRENT_ADDRESS          0x00
+#define MEMORY_VOLTAGE_ADDRESS          0x04
+#define MEMORY_CURRENT_LIMIT_ADDRESS    0x08
+#define MEMORY_BACKLIGHT_ADDRESS        0x0C
